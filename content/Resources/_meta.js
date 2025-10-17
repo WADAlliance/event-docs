79ff630841ae425853b8792295d743ab956ea3e6
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2025-10-17 20:57:30 +0200

    force re-build of hub page name

--- content/Resources/_meta.js
+++ content/Resources/_meta.js
@@ -2,6 +2,6 @@ export default {
     faqs: 'FAQs',
     hackathonDifference: 'Why is this hackathon so?',
     selectionCriteria: 'Hub Selection Criteria',
-    hubs: 'What is a hub?',
+    whatIsHub: 'What is a hub?',
     roadmap: 'Roadmap',
   }
